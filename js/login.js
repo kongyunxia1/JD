3c6db6b9d63085ab3fd96f58e62ff4c8c0ff5040
kyx <1753524294@qq.com>
2020-11-07 16:47:07 +0800

    11/7/2

---
+++ js/login.js
@@ -0,0 +1,9 @@
+$(".login_lef").click(function() {
+    $(".malogin").css({ "display": "block" })
+    $(".ZH_login").css({ "display": "none" })
+})
+
+$(".login_rig").click(function() {
+    $(".malogin").css({ "display": "none" })
+    $(".ZH_login").css({ "display": "block" })
+})
